f63d9a6fb766c8bb0f1328de91ed2fc2a91c050d
BerndAdamczyk <bernd_adamczyk@hotmail.de>
2022-08-19 08:42:19 +0200

    Changed Scene to build and clamped the rotation of GroundPlayer.

--- Assets/Scripts/GroundPlayerController.cs
+++ Assets/Scripts/GroundPlayerController.cs
@@ -10,6 +10,11 @@ public class GroundPlayerController : MonoBehaviour
     private float m_Multiplier;
 
     private Vector3 RotationVector;
+    private float m_XAxisRotation = 0f;
+    private float m_ZAxisRotation = 0f;
+    private float m_YAxisRotation = 0f;
+    private Vector3 m_StartRotation = Vector3.zero;
+    private Vector3 m_EndRotation = Vector3.zero;
     private void Awake()
     {
         m_PlayerInput = GetComponent<PlayerInput>();
@@ -29,12 +34,12 @@ public class GroundPlayerController : MonoBehaviour
                 //callback.ReadValue<Vector2>().y * m_Multiplier);
                 break;
             case InputActionPhase.Performed:
-                RotationVector = new Vector3(callback.ReadValue<Vector2>().y * m_Multiplier,
-                0,
-                -callback.ReadValue<Vector2>().x * m_Multiplier);
+                m_XAxisRotation = callback.ReadValue<Vector2>().y * m_Multiplier;
+                m_ZAxisRotation  = - callback.ReadValue<Vector2>().x * m_Multiplier;
                 break;
             case InputActionPhase.Canceled:
-                RotationVector = Vector3.zero;
+                m_XAxisRotation = 0f;
+                m_ZAxisRotation = 0f;
                 break;
             default:
                 break;
@@ -43,8 +48,10 @@ public class GroundPlayerController : MonoBehaviour
     }
     public void Update()
     {
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + RotationVector);
+        m_EndRotation.x = Mathf.Clamp(m_EndRotation.x + m_XAxisRotation, -50f, 50f);
+        m_EndRotation.z = Mathf.Clamp(m_EndRotation.z + m_ZAxisRotation, -50f, 50f);
 
-        
+        transform.rotation = Quaternion.Euler(m_EndRotation);
+
     }
 }
